0beeaa668125469991c49706811e1a388bd49c2f
Alessa <alesyavaskovith@gmail.com>
2023-02-28 19:20:06 +0200

    настроила API TelegramBot, обработка исключений, переход на страницу-благодарность

--- mami/groom/views.py
+++ mami/groom/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
 from .forms import *
+import telebot
+from django.core.exceptions import ValidationError
+
 
 menu = [
     {'title': 'Головна сторінка', 'url_name': 'home'},
@@ -13,11 +19,25 @@ def apply(request):
     if request.method == 'POST':
         form = Apply(request.POST)
         if form.is_valid():
-
             form.save()
+            send_telegram(form.cleaned_data)
+            return redirect(reverse('thanks'))
     else:
         form = Apply()
-    return render(request, 'groom/apply.html', {'form' : form})
+    return render(request, 'groom/apply.html', {'form': form})
+
+def send_telegram(data):
+    client_name = data['client_name']
+    city = data['city']
+    breed = data['breed']
+    phone_number = data['phone_number']
+
+    message = f"Новая заявка: {client_name}, {city}, {breed}, {phone_number}"
+
+    bot_token = "6160007408:AAHRFJQaKNsxD5l7Mdv1aHfjNDxZvvB3CG8"
+    chat_id = "-941286352"
+    bot = telebot.TeleBot(token=bot_token)
+    bot.send_message(chat_id=chat_id, text=message)
 
 def thanks(request):
     return render(request, 'groom/Thanks.html')
