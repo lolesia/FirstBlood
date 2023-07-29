f0a1b04eeee7a6947f41afb50a4c5920d18fbe26
lolesia <alesyavaskovith@gmail.com>
2023-07-29 12:03:44 +0300

    add user serializer, add test

--- mami/users/serializer.py
+++ mami/users/serializer.py
@@ -1,13 +1,42 @@
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from djoser.serializers import UserCreateSerializer
 from .models import User
 
+from phonenumbers import parse, is_valid_number, NumberParseException
 
-class CreateUserSerializer(UserCreateSerializer):
-    class Meta(UserCreateSerializer.Meta):
+
+class UserSerializer(serializers.ModelSerializer):
+
+    password1 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+    phone = serializers.CharField()
+
+    class Meta:
         model = User
-        fields = ('id', 'email', 'name', 'last_name', 'phone', 'password')
+        fields = ('email', 'name', 'last_name', 'phone', 'password1', 'password2')
+
+    def validate(self, data):
+        password1 = data.get('password1')
+        password2 = data.get('password2')
+
+        if password1 != password2:
+            raise serializers.ValidationError('Паролі не співпадають')
+        return data
+
+    def validate_phone(self, phone):
+        parsed_number = parse(phone, "UA")
+        if not is_valid_number(parsed_number):
+            raise serializers.ValidationError("Invalid phone number")
+        return phone
+
+    def create(self, validated_data):
+        email = validated_data.get('email')
+        name = validated_data.get('name')
+        last_name = validated_data.get('last_name')
+        phone = validated_data.get('phone')
+        password1 = validated_data.get('password1')
+
+        user = User.objects.create_user(email=email, name=name, last_name=last_name, phone=phone, password=password1)
+        return user
 
 
 
--- mami/users/tests.py
+++ mami/users/tests.py
@@ -1,2 +1,44 @@
 from django.test import TestCase
+from rest_framework import serializers
+from .serializer import UserSerializer
+
+
+class UserSerializerTest(TestCase):
+    data_positve = {
+        'email': 'test@example.com',
+        'name': 'John',
+        'last_name': 'Doe',
+        'phone': '+380634002895',
+        'password1': 'password123',
+        'password2': 'password123',
+    }
+
+    def test_serializer(self):
+
+        serializer = UserSerializer(data=UserSerializerTest.data_positve)
+        self.assertTrue(serializer.is_valid())
+
+    def test_validate_phone(self):
+        valid_phone = '0634002895'
+        serializer = UserSerializer()
+        try:
+            validated_phone = serializer.validate_phone(valid_phone)
+            self.assertEquals(validated_phone, valid_phone)
+        except serializers.ValidationError:
+            self.fail("Валідація номеру телефона не повинаа викликати виключення")
+
+
+    def test_validate_password(self):
+        valid_password = {'password1': 'password123',
+                          'password2': 'password123'}
+        serializer = UserSerializer()
+        try:
+            validated_data = serializer.validate(valid_password)
+            self.assertEqual(validated_data, valid_password)
+        except serializers.ValidationError:
+            self.fail("Валідація паролю не повинаа викликати виключення")
+
+
+
+
 
